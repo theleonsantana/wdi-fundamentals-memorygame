a1585a9364fb995f6dcd68444e1f74a8a58bed27
Leonardo Santana <hi@leonsantana.com>
2019-08-21 23:00:17 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+// card var
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardFour);
