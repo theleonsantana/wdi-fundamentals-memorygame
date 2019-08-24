3bb9d3097e066ba4d2db60dc606531a4399f83aa
Leonardo Santana <hi@leonsantana.com>
2019-08-23 20:18:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,16 @@
-// card var
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+// CARD ARRAY
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+const cardTwo = cards[1];
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardFour);
+cardsInPlay.push(cardOne, cardTwo);
+
+if (cardsInPlay.length === 2) {
+  console.log('2');
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  } else {
+    alert('Sorry try again!');
+  }
+}
